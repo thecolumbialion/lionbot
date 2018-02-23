091e6278a919004e594cd861515335062839c6d6
Chang Liu <cl3403@columbia.edu>
2018-02-23 16:40:24 -0500

    1.used pickle to save the dictionary; 2. changed what find_clubs returns

--- packages/clubs/clubs.py
+++ packages/clubs/clubs.py
@@ -1,5 +1,6 @@
 import os 
 import csv
+import pickle
 
 def clubs_msg(result):
 	try:
@@ -16,11 +17,16 @@ def make_club_dict():
 		if len(club_info) < 2: 
 			continue
 		club_dict[club_info[0].strip()] = club_info[1].strip()
-	return club_dict
+	club_file.close()
+	output_file = open("./club_dict", "wb")
+	pickle.dump(club_dict, output_file)
+	output_file.close()
 
 def find_clubs(club):
 	club = club.lower()
-	clubs_dict = make_club_dict()
+	input_file = open("./club_dict", "rb")
+	clubs_dict = pickle.load(input_file)
+	input_file.close()
 	results = []
 	response = ""
 	for club_name in clubs_dict.keys():
@@ -29,6 +35,19 @@ def find_clubs(club):
 			club_result = club_name + ": " + clubs_dict[club_name] 
 			results.append(club_result)
 
+	"""
+
+	- Modification: now find_clubs returns a list of strings instead of a single string
+	- If the list is empty: no result is found
+	- If the list has multiple entries: any one of them might be the club the user is looking for. In that case, the bot 
+	  should handle the logic to check with the users which club is the one they want. 
+
+
+	"""
+
+	return results
+
+	"""
 	if len(results) == 0:
 		msg = "Looks like I couldn't find any information about that club."
 		return msg
@@ -38,4 +57,5 @@ def find_clubs(club):
 			response += line + "\n"
 	response = response.rstrip()
 	return response
+	"""
 
