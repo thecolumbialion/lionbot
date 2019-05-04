89d4adec474da2939e6b2005f220dde0d74cba95
Lord Crawford <lrc2161@columbia.edu>
2019-05-03 20:11:13 -0400

    did event stuff

--- packages/academic/campus_events.py
+++ packages/academic/campus_events.py
@@ -9,23 +9,23 @@ import itertools
 
 
 def campus_events_msg(result):
-    return current_events()
+    return current_events(5)
 
 
-#takes in output from current_events function
-def event_finder():
-    return
+# #takes in output from current_events function
+# def event_finder():
+#     return current_events(5)
 
 
 #returns a dictionary mapping dates to events
-def current_events():
+def current_events(count):
 
     url = 'https://events.columbia.edu/feeder/main/eventsFeed.do?f=y&sort=dtstart.\
     utc:asc&fexpr=(categories.href!=%22/public/.bedework/categories/sys/Ongoing%22)%\
     20and%20(categories.href=%22/public/.bedework/categories/org/UniversityEvents%22)\
-    %20and%20(entity_type=%22event%22%7Centity_type=%22todo%22)&skinName=list-json&count=10'\
+    %20and%20(entity_type=%22event%22%7Centity_type=%22todo%22)&skinName=list-json&count='\
 
-    file = requests.get(url)
+    file = requests.get(url + str(count))
     file_json = file.json()
 
 
@@ -35,10 +35,10 @@ def current_events():
     start_times = [li['start'] for li in event_list]
     end_times = [li['end'] for li in event_list]
 
-    event_msg = "Here are some 10 events going on today or in the near future, \n \n"
+    event_msg = "Here are some " + str(count) + " events going on today or in the near future, \n \n"
 
     for (event, start, end) in zip(events, start_times, end_times):
-     event_msg += 'Name: ' + event + '\n'+ 'Start Date: ' + start['shortdate'] + " " + start['time'] + ' End Date: ' + end['shortdate'] + " " + end['time'] + '\n\n'
+     event_msg += 'Name: ' + event + '\n'+ 'From ' + start['shortdate'] + ' to ' + end['shortdate'] + '\n\n'
 
 
     return(event_msg)
@@ -48,7 +48,7 @@ def current_events():
 
 
 if __name__ == '__main__':
-    current_events()
+    current_events(5)
 
 """
 next time add onto this feature
